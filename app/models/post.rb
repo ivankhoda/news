00d4bd7971b83911f924c6747e66f5c6c8c2ce23
Ivan Khoda <ivankhoda@gmail.com>
2022-08-01 09:06:58 +0300

    Make fields required

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   attribute :visible, :boolean, default: -> { true }
   validates :title, presence: true
-  validates :text, presence: true
+  validates :content, presence: true
   validates :link, presence: true
 end
